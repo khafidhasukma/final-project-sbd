cf01ce3e0f1c3aec217d1130476d32ceca6b479b
Khafidha Sukma <khafidhasukma@gmail.com>
2025-06-19 12:04:02 +0700

    feat: add initial structure

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Inventory App</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+  <style>
+  /* Fonts */
+  @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+
+  body {
+    font-family: 'Plus Jakarta Sans', sans-serif;
+  }
+  </style>
+
+</head>
+
+<body>
+  <!-- Navbar -->
+  <?php include 'navbar.php'; ?>
+
+  <div class="container">
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once 'components/header.php'; ?>
+
+<!-- content -->
+<div>
+  <p>halo</p>
+</div>
+<?php include_once 'components/footer.php'; ?>
